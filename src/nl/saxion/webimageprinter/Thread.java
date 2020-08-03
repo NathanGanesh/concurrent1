99f86bf24506d9a2aa2bb9501d18583a9f42e310
nathanganesh <nathangemieee@gmail.com>
2020-08-03 22:07:27 +0200

    1.2 af

---
+++ src/nl/saxion/webimageprinter/Thread.java
@@ -0,0 +1,36 @@
+package nl.saxion.webimageprinter;
+
+import java.awt.*;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Thread  extends java.lang.Thread {
+    int k;
+    String webImage2;
+
+    WebImage image;
+
+    public Thread(String webImage, int i) {
+            k = i;
+            this.webImage2 = webImage;
+
+    }
+
+    public WebImage getImage() {
+        return image;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Thread no. "+k);
+        try {
+//            webImages.add(new WebImage(webImage2));
+            image = (new WebImage(webImage2));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
--- src/nl/saxion/webimageprinter/WebImagePrinter.java
+++ src/nl/saxion/webimageprinter/WebImagePrinter.java
@@ -8,17 +8,74 @@ import java.net.URL;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
-
-public class WebImagePrinter {
-
+public class WebImagePrinter{
 
     public static void main(String[] args) {
         Instant first = Instant.now();
         new WebImagePrinter().print("http://concurrency.anthonyvandenberg.nl/");
+        Refresh("http://concurrency.anthonyvandenberg.nl/");
+
         Instant second = Instant.now();
         Duration duration = Duration.between(first, second);
-        System.out.println("Total duration: seconds"+duration.getSeconds()+ "Total Nano: "+duration.getNano());
+        System.out.println("Total seconds: "+duration.getSeconds());
     }
+
+    public static void Refresh(String location){
+        ArrayList<String> images = getImageList(location);
+        ArrayList<WebImage> webImages = new ArrayList<WebImage>();
+        ArrayList<Thread> threads = new ArrayList<>();
+        int threadIndex =1;
+        for (String image: images){
+            System.out.println("Loading " + image);
+            Thread temp = new Thread((location + "/" + image), threadIndex);
+            threads.add(temp);
+            threadIndex++;
+            temp.start();
+            try {
+                temp.join(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+                webImages.add(thread.getImage());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        PdfImageWriter pdfwriter = new PdfImageWriter(webImages);
+        pdfwriter.printToPdf("test.pdf");
+
+    }
+
+
+    /**
+     *
+     * @param location
+     * Todo: getting the images sequentially takes a lot of time.
+     * Change the code so that each image is loaded in its own thread
+     */
+    public void print(String location) {
+        ArrayList<String> images = getImageList(location);
+        ArrayList<WebImage> webImages = new ArrayList<WebImage>();
+        for(String image : images )
+        {
+            try {
+                System.out.println(images.size());
+                System.out.println("Loading " + image);
+                webImages.add(new WebImage(location + "/" + image));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        PdfImageWriter pdfwriter = new PdfImageWriter(webImages);
+        pdfwriter.printToPdf("dogga.pdf");
+     }
 
 
     /**
@@ -28,7 +85,7 @@ public class WebImagePrinter {
      *
      * Reads a list of images from a web server.
      */
-    private ArrayList<String> getImageList(String location) {
+    private static ArrayList<String> getImageList(String location) {
 
         ArrayList<String > images = new ArrayList<>();
         try {
@@ -47,28 +104,5 @@ public class WebImagePrinter {
     }
 
 
-    /**
-     *
-     * @param location
-     * Todo: getting the images sequentially takes a lot of time.
-     * Change the code so that each image is loaded in its own thread
-     */
-    public void print(String location) {
-        ArrayList<String> images = getImageList(location);
-        ArrayList<WebImage> webImages = new ArrayList<WebImage>();
-        for(String image : images )
-        {
-            try {
-                System.out.println("Loading " + image);
-                webImages.add(new WebImage(location + "/" + image));
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        PdfImageWriter pdfwriter = new PdfImageWriter(webImages);
-        pdfwriter.printToPdf("test.pdf");
-     }
 
 }
